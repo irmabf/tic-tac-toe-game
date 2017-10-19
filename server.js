6f4a718815e50548f7480631e42485e55ee5471d
Priscila Oliveira <Priscila.Oliveira@ataccama.com>
2017-10-19 17:22:54 +0200

    moved server.js file, removed open library, modified scripts

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 8080
+const app = express()
+
+app.use(express.static(`${__dirname}/dist`))
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, './dist', 'index.html'))
+})
+
+app.listen(port, (err) => {
+	if (err) {
+		console.log(err)
+	} else {
+		console.log(`http://localhost:${port}`)
+	}
+})
